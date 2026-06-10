ddb4afe9960bf78f6989ed3017e0f15006e9182d
Lucas-Ibrahim <lucas.ibrahimfwb@gmail.com>
2026-06-10 19:39:34 -0300

    2o dia de python

---
+++ Dia2/variaveis.py
@@ -0,0 +1,101 @@
+# Dia 2 de 30 de python
+
+# Declarando variaveis
+
+primeiro_nome = 'Lucas'
+sobrenome = "Ibrahim"
+nome_completo = 'Lucas Ibrahim'
+pais = 'Brasil'
+cidade = 'Sao Paulo'
+idade = 20
+ano = 2026
+is_married = False
+is_true = True
+is_light_on = True
+
+dia, mes, semestre, estado = 10, 'junho', '1o', 'SP'
+
+# imprimindo variaveis
+
+print(primeiro_nome)
+print(sobrenome)
+print(nome_completo)
+print(pais)
+print(cidade)
+print(idade)
+print(ano)
+print(is_married)
+print(is_true)
+print(is_light_on)
+print(dia)
+print(mes)
+print(semestre)
+print(estado)
+
+# Verifique o tipo de dados de todas as suas variáveis ​​usando a função integrada type()
+
+print(type(primeiro_nome))
+print(type(sobrenome))
+print(type(nome_completo))
+print(type(pais))
+print(type(cidade))
+print(type(idade))
+print(type(ano))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(dia))
+print(type(mes))
+print(type(semestre))
+print(type(estado))
+
+print(len(primeiro_nome))
+print(len(sobrenome))
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+print(f'total = {total}')
+diff = num_two - num_one
+print(f'diff = {diff}')
+produto = num_one * num_two
+print(f'produto = {produto}')
+divisao = num_one / num_two
+print(f'divisao = {divisao}')
+restante = num_two % num_one
+print(f'restante = {restante}')
+exp = num_one ** num_two
+print(f'exp = {exp}')
+floor_division = num_one // num_two
+print(f'floor_division = {floor_division}')
+
+# Area do circulo = pi * r^2
+
+area_of_circle = 3.14 * 3.091**2
+print(f'Area = {area_of_circle}')
+
+# Circunferencia 2 * pi * r
+
+circum_of_circle = 2 * 3,14 * 3.091
+print(f'Circunferecia = {circum_of_circle}')
+
+# Dados usuario
+
+nome_user = input('Digite seu nome: ')
+sobrenome_user = input('Digite seu sobrenome: ')
+pais_user = input('Digite seu pais: ')
+idade_user = input('Digite sua idade: ')
+
+print(nome_user)
+print(sobrenome_user)
+print(pais_user)
+print(idade_user)
+
+
+
+
+
+
+
+
